c810ac142f11bf4711ed0b22b6784ea34b03caa5
Tarun Ergurala <tarunergurala7030@gmail.com>
2023-03-10 21:08:58 +0530

    completed

--- Day-3/src/component/TodoItems.jsx
+++ Day-3/src/component/TodoItems.jsx
@@ -2,8 +2,8 @@ function TodoItem({ id, title, status, handleToggle, handleDelete }) {
   return (
     <li key={id}>
       {title} - {status ? "Completed" : "Not Completed"}
-      <button onClick={() => handleToggle(id)}>TOGGLE</button>
-      <button onClick={() => handleDelete(id)}>DELETE</button>
+      <button onClick={() => handleToggle(id)}>Toggle</button>
+      <button onClick={() => handleDelete(id)}>Delete</button>
     </li>
   );
 }
